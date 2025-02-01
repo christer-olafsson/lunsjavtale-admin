573f8360c53b940788b522611531649489d163ce
hamim <hamim2114@gmail.com>
2025-02-01 23:31:54 +0600

    updated

--- src/pages/suppliers/SupplierDetails.jsx
+++ src/pages/suppliers/SupplierDetails.jsx
@@ -119,7 +119,7 @@ const SupplierDetails = () => {
                   <Stack gap={2} mt={{ xs: 3, md: 0 }}>
                     {[
                       { label: 'Sold Amount', value: `${vendor?.soldAmount ?? '00'} kr`, bgColor: 'RebeccaPurple' },
-                      { label: 'Commission', value: `${vendor?.commission ?? '0'} %`, bgColor: 'blue' },
+                      { label: 'Lunsjavtale Commission', value: `${vendor?.commission ?? '0'} %`, bgColor: 'blue' },
                       { label: 'Lunsjavtale Commission', value: `${vendor?.ownerCommission ?? '0'} kr`, bgColor: 'blue' },
                       { label: 'Withdrawn Amount', value: `${vendor?.withdrawnAmount ?? '00'} kr`, bgColor: 'purple' },
                       { label: 'Balance', value: `${vendor?.balance ?? '00'} kr`, bgColor: 'green' },

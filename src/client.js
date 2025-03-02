de09b13e1c88f6c38c499b656ce2a9415a4414fe
hamim <hamim2114@gmail.com>
2025-03-02 23:21:30 +0600

    updated

--- src/client.js
+++ src/client.js
@@ -26,7 +26,7 @@ const errorLink = onError(
         console.log('graphql err:',message)
         if (
           message === "Signature has expired" ||
-          code === "unauthorized"
+          extensions.code === "unauthorized"
         ) {
           localStorage.removeItem('admin_lunsjavtale');
           // localStorage.removeItem("refresh");

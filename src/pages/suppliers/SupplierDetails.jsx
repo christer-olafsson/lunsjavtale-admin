c4aa5eee8ee383accd17c59858ca98482293eadf
hamim <hamim2114@gmail.com>
2025-03-07 16:08:03 +0600

    updated

--- src/pages/suppliers/SupplierDetails.jsx
+++ src/pages/suppliers/SupplierDetails.jsx
@@ -92,6 +92,7 @@ const SupplierDetails = () => {
                         </Typography>
                       )}
                       <Typography><b>Contact: </b>{vendor?.contact}</Typography>
+                      <Typography><b>Total Commission: </b><span style={{ color: 'green' }}>{vendor?.ownerCommission ?? '0'}</span> kr</Typography>
                       <Typography mt={2} mb={1}><b>Post Codes:</b></Typography>
                       <Stack direction="row" gap={1.5} flexWrap="wrap">
                         {vendor?.postCode?.map((p) => (
@@ -120,7 +121,7 @@ const SupplierDetails = () => {
                     {[
                       { label: 'Sold Amount', value: `${vendor?.soldAmount ?? '00'} kr`, bgColor: 'RebeccaPurple' },
                       // { label: 'Lunsjavtale Commission', value: `${vendor?.commission ?? '0'} %`, bgColor: 'blue' },
-                      { label: 'L Commission', value: `(${vendor?.commission}%) ${vendor?.ownerCommission ?? '0'} kr`, bgColor: 'blue' },
+                      { label: 'Current Commission', value: `(${vendor?.commission}%)`, bgColor: 'blue' },
                       { label: 'Withdrawn Amount', value: `${vendor?.withdrawnAmount ?? '00'} kr`, bgColor: 'purple' },
                       { label: 'Balance', value: `${vendor?.balance ?? '00'} kr`, bgColor: 'green' },
                     ].map((item, index) => (
